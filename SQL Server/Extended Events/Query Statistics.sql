48858b049285b78b0dfe2f623142a06caff3a2ca
Rafael Rodrigues <rafael@datageek.com.br>
2024-04-24 14:58:01 -0300

    Update Query Statistics.sql

--- SQL Server/Extended Events/Query Statistics.sql
+++ SQL Server/Extended Events/Query Statistics.sql
@@ -110,6 +110,11 @@ WITH ( MAX_MEMORY = 4096 KB ,
         MEMORY_PARTITION_MODE = NONE ,
         TRACK_CAUSALITY = ON ,
         STARTUP_STATE = OFF )
+/*
+ADD TARGET package0.event_file(SET filename=N'D:\rdsdbdata\Log\QueryBaseline.xel',max_file_size=(100),max_rollover_files=(5))
+WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=30 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=ON,STARTUP_STATE=ON)
+GO
+*/
 GO
 
 /* Inicialização / paralização da sessão */
